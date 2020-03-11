3fcbdc59274863ef497ce8b35a213c4be8c033ab
Mckay <214697893@qq.com>
2020-03-11 17:20:54 +0800

    重新提交1

---
+++ LeetcodePracticeForJava/src/com/mk/demo/Closure.java
@@ -0,0 +1,9 @@
+package com.mk.demo;
+
+/**
+ * @author MengKai
+ * @create 2020-03-11
+ */
+public interface Closure {
+    void func1();
+}
---
+++ LeetcodePracticeForJava/src/com/mk/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.mk.demo;
+
+/**
+ * @author MengKai
+ * @create 2020-03-11
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
